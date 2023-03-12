d0242dce1a29307aa35f9315fcf2c82210351006
ThunderfuryofWindseeker <nedyalkov1717@gmail.com>
2023-03-12 23:03:51 +0200

    .

---
+++ Course-2/project/src/App.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+
+import './styles/app.css';
+import PostItem from './components/item';
+import PostList from './components/postList';
+function App() {
+  const [posts, setPosts] = useState([
+    {id: 1, title:'JavaScript', body:'Description'},
+    {id: 2, title:'JavaScript 2', body:'Description'},
+    {id: 3, title:'JavaScript 3', body:'Description'}
+  ]);
+  const [posts2, setPosts2] = useState([
+    {id: 1, title:'Python', body:'Description'},
+    {id: 2, title:'Python 2', body:'Description'},
+    {id: 3, title:'Python 3', body:'Description'}
+  ]);
+  return (
+    <div className="App">
+      <PostList posts = {posts} title="Posts about JS"/>
+      <PostList posts = {posts2} title="Posts about Python"/>
+    </div>
+  );
+}
+
+export default App;
